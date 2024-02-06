b036918fdf45ddab9a09f4c5e950b9d729177a8c
nllanos-applica <nllanos@applica.dev>
2024-02-06 13:10:16 -0300

    advances

--- Indotica/Indotica/ViewController.swift
+++ Indotica/Indotica/ViewController.swift
@@ -8,9 +8,11 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVCaptureMetadataOutputObjectsDelegate {
 
     @IBOutlet weak var loginWithQrButton: UIButton!
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,5 +40,49 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
+    }
+
+    func setupCamera() {
+        captureSession = AVCaptureSession()
+
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+            print("No se encontró una cámara disponible")
+            return
+        }
+
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(input)
+        } catch {
+            print("Error al configurar la entrada de la cámara: \(error.localizedDescription)")
+            return
+        }
+
+        let output = AVCaptureMetadataOutput()
+        captureSession.addOutput(output)
+
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        output.metadataObjectTypes = [.qr]
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = .resizeAspectFill
+        previewLayer.frame = view.layer.bounds
+        view.layer.addSublayer(previewLayer)
+
+        captureSession.startRunning()
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        guard let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
+              let qrCodeValue = metadataObject.stringValue else {
+            print("No se pudo obtener el valor del código QR")
+            return
+        }
+        print("Código QR escaneado: \(qrCodeValue)")
+
+        // Aquí puedes realizar las acciones necesarias con el valor del código QR
+
+        captureSession.stopRunning()
+        previewLayer.removeFromSuperlayer()
     }
 }
