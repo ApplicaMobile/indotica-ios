4aa83bdebcde0ca14bcbc336f86cb0c959dd32e8
NicolasLlanos8799 <nicollanos8799@gmail.com>
2024-02-07 14:52:18 -0300

    camara funcionando y mostrando data obtenida

--- Indotica/Indotica/ViewController.swift
+++ Indotica/Indotica/ViewController.swift
@@ -8,81 +8,99 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVCaptureMetadataOutputObjectsDelegate {
-
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
     @IBOutlet weak var loginWithQrButton: UIButton!
     var captureSession: AVCaptureSession!
     var previewLayer: AVCaptureVideoPreviewLayer!
-
+    var alert: UIAlertController?
+    private var borderView = UIView(frame: CGRect(x: 0, y: 0, width: 90, height: 90))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        loginWithQrButton.layer.borderWidth = 3.0
-        loginWithQrButton.layer.cornerRadius = 15.0
-        loginWithQrButton.layer.borderColor = UIColor.black.cgColor
     }
-
-    @IBAction func loginWithQrButtonAction(_ sender: Any) {
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let imagePicker = UIImagePickerController()
-            imagePicker.sourceType = .camera
-            imagePicker.delegate = self
-
-            present(imagePicker, animated: true, completion: nil)
-        } else {
-            print("El dispositivo no tiene cámara.")
-        }
-    }
-
-    @objc func backButtonAction() {
-        dismiss(animated: true, completion: nil)
-    }
-
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
-    }
-
-    func setupCamera() {
+    
+    func setupScanning() {
+        // Crear una sesión de captura
         captureSession = AVCaptureSession()
-
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
-            print("No se encontró una cámara disponible")
+        
+        // Obtener el dispositivo de captura de video predeterminado
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            failed()
             return
         }
-
+        
+        // Crear un objeto de entrada de video con el dispositivo de captura
         do {
-            let input = try AVCaptureDeviceInput(device: captureDevice)
-            captureSession.addInput(input)
+            let videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+            
+            // Agregar la entrada de video a la sesión de captura
+            if captureSession.canAddInput(videoInput) {
+                captureSession.addInput(videoInput)
+            } else {
+                failed()
+                return
+            }
         } catch {
-            print("Error al configurar la entrada de la cámara: \(error.localizedDescription)")
+            failed()
             return
         }
-
-        let output = AVCaptureMetadataOutput()
-        captureSession.addOutput(output)
-
-        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-        output.metadataObjectTypes = [.qr]
-
+        
+        // Crear un objeto de salida de metadatos
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        // Agregar la salida de metadatos a la sesión de captura
+        if captureSession.canAddOutput(metadataOutput) {
+            captureSession.addOutput(metadataOutput)
+            
+            // Configurar el delegado y los tipos de objetos de metadatos
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            failed()
+            return
+        }
+        
+        // Configurar la vista previa de video
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.videoGravity = .resizeAspectFill
         previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        
+        // Agregar la vista previa de video a la vista actual
         view.layer.addSublayer(previewLayer)
+    }
 
+    
+    @IBAction func loginWithQrButtonAction(_ sender: Any) {
+        setupScanning()
         captureSession.startRunning()
     }
-
+    
+    
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+    
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        guard let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
-              let qrCodeValue = metadataObject.stringValue else {
-            print("No se pudo obtener el valor del código QR")
-            return
-        }
-        print("Código QR escaneado: \(qrCodeValue)")
-
-        // Aquí puedes realizar las acciones necesarias con el valor del código QR
-
         captureSession.stopRunning()
-        previewLayer.removeFromSuperlayer()
+        
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            getData(code: stringValue)
+        }
+    }
+    
+    func getData(code: String) {
+        alert = UIAlertController(title: "QR Code", message: code, preferredStyle: .alert)
+        alert?.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            self.captureSession.startRunning()
+            self.alert?.dismiss(animated: true, completion: nil)
+        }))
+        present(alert ?? UIAlertController(), animated: true)
     }
 }
